ed2825036f475b109ba0b7d6637d7d8b9ad77458
Yuta Kitagawa <y.kitagawa.r@outlook.jp>
2017-07-20 12:39:15 +0900

    TV操作機能を追加

--- arduino_led/js/index.js
+++ arduino_led/js/index.js
@@ -1,29 +1,66 @@
-var PHP_URL = "ledSet.php"
+var PHP_URL = "statusSet.php"
 
 $(function(){
   init();
 });
 //初期処理
 function init(){
-    offLed();
-  $('#on_btn').click(function(e){
-    onLed();
+  $('#led_main_area').show();
+  $('#tv_main_area').hide();
+
+  ledInit();
+  tvInit();
+
+  $('#led_show_button').click(function(e){
+    setTimeout(function(){
+      $('#led_main_area').fadeIn(1000);
+    },300);
+    $('#tv_main_area').fadeOut(250);
   });
-  $('#off_btn').click(function(e){
-    offLed();
+  $('#tv_show_button').click(function(e){
+    $('#led_main_area').fadeOut(250);
+    setTimeout(function(){
+      $('#tv_main_area').fadeIn(1000);
+    },300);
+  });
+  $('#humidity_show_button_area').click(function(e){
+    alert("comming soon!!")
+  });
+
+
+}
+function ledInit(){
+  $('#on_led').hide();
+  $('#off_led').show();
+  sendStatus("LED_OFF");
+
+  $('#on_led_btn').click(function(e){
+    $('#on_led').show();
+    $('#off_led').hide();
+    sendStatus("LED_ON");
+  });
+  $('#off_led_btn').click(function(e){
+    $('#on_led').hide();
+    $('#off_led').show();
+    sendStatus("LED_OFF");
   });
 }
-function onLed(){
-  $('#off').hide();
-  $('#on').show();
-  sendLedStatus("ON");
+function tvInit(){
+  $('#on_tv').hide();
+  $('#off_tv').show();
+
+  $('#on_tv_btn').click(function(e){
+    $('#on_tv').show();
+    $('#off_tv').hide();
+    sendStatus("TV_ON");
+  });
+  $('#off_tv_btn').click(function(e){
+    $('#on_tv').hide();
+    $('#off_tv').show();
+    sendStatus("TV_OFF");
+  });
 }
-function offLed(){
-  $('#on').hide();
-  $('#off').show();
-  sendLedStatus("OFF");
-}
-function sendLedStatus(status){
+function sendStatus(status){
   $.ajax({
     type: "post",
     url: PHP_URL,
--- arduino_led/statusSet.php
+++ arduino_led/statusSet.php
@@ -2,8 +2,9 @@
 header("Content-type: application/json; charset=utf8");
 if(isset($_POST['status'])){
   $status = $_POST['status'];
-  file_put_contents("ledLog.txt","led-status:".$status);
+  file_put_contents("statusLog.txt","status:".$status);
   echo json_encode($status);
+
 }else{
   $error = "失敗";
   echo json_encode($error);
